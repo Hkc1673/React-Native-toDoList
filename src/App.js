143cee1af75a01eaa10a273c15c06c9972765a05
Hkc-1673 <habibkc71@gmail.com>
2020-10-14 00:07:25 +0300

    init files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import React from 'react';
+import Main from "./Main";
+import Note from './note';
+
+export default class App extends React.Component {
+  render(){
+    return (
+      <Main/>
+
+    );
+  } 
+}
