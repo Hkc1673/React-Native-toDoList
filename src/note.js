5f8c2dd3623926d74f31af6189737361be547139
Hkc-1673 <habibkc71@gmail.com>
2020-10-15 00:17:01 +0300

    Modified Note.js

--- src/note.js
+++ src/note.js
@@ -10,7 +10,6 @@ export default class Note extends React.Component {
     render() {
         return (
             <View key={this.props.keyval} style={styles.note}>
-                <Text style={styles.noteText}>{this.props.val.date}</Text>
                 <Text style={styles.noteText}>{this.props.val.note}</Text>
                 
 
@@ -32,9 +31,11 @@ const styles = StyleSheet.create({
         borderBottomColor: '#ededed',
     },
     noteText: {
-        paddingLeft: 20,
-        borderLeftWidth: 10,
-        borderLeftColor: '#e91e63',
+        padding: 20,
+        backgroundColor: "#546E7A",
+        color: "white",
+        borderRadius: 10,
+        fontSize: 20,
     },
     noteDelete: {
         position: 'absolute',
