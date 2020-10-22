7458e7bbcb7e7d83c78ef06292bc8ab01b824da1
Hkc-1673 <habibkc71@gmail.com>
2020-10-22 17:19:53 +0300

    project completed with function component

--- src/Main.js
+++ src/Main.js
@@ -1,137 +1,69 @@
+import React, {useState} from "react";
+import { SafeAreaView, Text, View, FlatList } from 'react-native';
 
-import React from 'react';
-import {
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  TextInput,
-  TouchableOpacity,
-  Dimensions,
-} from 'react-native';
-import Note from "./Note";
+import { main } from './styles';
+import { TodoInput, TodoCard } from './components';
 
+const Main = () => {
+    const [list, setList] = useState([])
 
-
-export default class Main extends React.Component  {
-
-    constructor(props){
-      super(props);
-
-      this.state = {
-        noteArray:[],
-        noteText:"",
-      }
+    function addTodo(text) {
+        const element = {
+            id: list.length,
+            todo: text,
+            isDone: false
+        }
+        const newArray = [element, ...list]
+        setList(newArray);
     }
 
-   
+    function doneTodo(todoId) {
+        const newArray = [...list];
+        const todoIndex = newArray.findIndex(item => item.id == todoId);
 
-    render(){
+        newArray[todoIndex].isDone = !newArray[todoIndex].isDone;
 
-        let notes = this.state.noteArray.map((val,key)=>{
-          return <Note key={key} keyval={key} val={val}
-                  deleteMethod={()=> this.deleteNote(key)}/>
-        });
+        setList(newArray);
+    }
 
+    function removeTodo(todoId) {
+        const newArray = [...list];
+        const todoIndex = list.findIndex(t => t.id == todoId);
+
+        newArray.splice(todoIndex, 1);
+        setList(newArray);
+    }
+
+    const renderTodo = ({ item }) => {
         return (
-            <View style= {styles.container}>
-              <View style={styles.header}>
-                <Text style={styles.headerText}>TODO</Text>
-                <Text style={{ fontSize: 50, color: '#FFA726'  }}>{notes.length}</Text>
-              </View>
+            <TodoCard
+                data={item}
+                onDone={() => doneTodo(item.id)}
+                onRemove={() => removeTodo(item.id)}
+            />
+        )
+    }
 
-              <ScrollView style={styles.scrollContainer}>
-                {notes}
-              </ScrollView> 
+    return (
+        <SafeAreaView style={main.container}>
 
-              <View style={styles.footer}>
-                <TextInput
-                 style={styles.textInput}
-                 onChangeText={(noteText)=> this.setState({noteText})}
-                 value={this.state.noteText}
-                 placeholder="Write New Task..."
-                 underlineColorAndroid="transparent">
-                </TextInput>
-              
-
-                <TouchableOpacity onPress={this.addNote.bind(this)}
-                style={styles.addButton}>
-                  <Text style={styles.addButtonText}>ADD TO DO</Text>
-                </TouchableOpacity>
+                <View style={main.banner}>
+                    <Text style={main.todoText}>TODO</Text>
+                    <Text style={main.todoCount}>{list.filter(t => t.isDone === false).length}</Text>
                 </View>
-              
-            </View>
-          );
-    }
-    addNote() {
-      if (this.state.noteText) {
 
-        this.state.noteArray.push({
-          "note":this.state.noteText
-        });
+                <FlatList
+                    keyExtractor={(item, index) => index.toString()}
+                    data={list}
+                    renderItem={renderTodo}
+                    ListEmptyComponent={() => <Text style={main.emptyComponent}>Nothing to do..</Text>}
+                />
 
-        this.setState({noteArray: this.state.noteArray});
-        this.setState({noteText: ""});
-      }
-    }
+                <TodoInput
+                    onTodoEnter={todoText => addTodo(todoText)}
+                />
+        </SafeAreaView>
+    );
+}
 
-    deleteNote(key){
-        this.state.noteArray.splice(key,1);
-        this.setState({noteArray: this.state.noteArray});
-    }
-};
-
-
-
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#37474F",
-  },
-  header: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 20,
-  },
-  headerText: {
-    color: '#FFA726',
-    fontWeight:"bold",
-    fontSize: 50,
-  },
-  scrollContainer: {
-    flex: 1,
-  },
-  footer: {
-    height:180,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: "#B0BEC5",
-    margin:10,
-    borderRadius: 20,
-
-  },
-  textInput: {
-    color: 'black',
-    padding: 20,
-    backgroundColor: '#ECEFF1',
-    borderRadius: 20,
-    margin: 20,
-    marginBottom: 10,
-    width: Dimensions.get("window").width*0.85,
-  },
-  addButton: {
-    backgroundColor: '#546E7A',
-    width: 250,
-    height: 60,
-    borderRadius: 10,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  addButtonText: {
-    color: '#fff',
-    fontSize: 24,
-  },
-});
-
+export default Main;
